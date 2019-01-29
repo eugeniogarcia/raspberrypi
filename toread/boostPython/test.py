5ec2723a619c8c2a06c62b053b4a577e8c35bce0
Eugenio <egsmartin@hotmail.com>
2019-01-29 07:19:06 +0000

    Cap 6

--- toread/boostPython/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# A Python program that calls C program code
-import erpi
-
-print "Start of the Python program"
-print erpi.hello("Derek")
-val = erpi.integrate(0, 3.14159, 1000000)
-print "The integral result is: ", val
-print "End of the Python program"
--- toread/proc/whologgedin.c
+++
@@ -1,8 +0,0 @@
-/* Written by Derek Molloy for the book "Exploring RPi */ 
-
-#include<unistd.h>
-#include<stdio.h>
-int main(){
-   printf("The user logged in is %s\n", getlogin());
-   return 0;
-}
